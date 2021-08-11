a9b021bb573cf6a3036043253b6927bdcc9244f7
Anatoly Bolgov <bolgov.anatoli@gmail.com>
2021-08-11 09:50:28 +0700

    dist

---
+++ dist/precache-manifest.761d21b253fe193615498d722def7365.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f465dcb24bf57f70020a",
+    "url": "/css/app.0e88525e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cb8269b47657915a195a323e15237382",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3b4fc58700026b9098",
+    "url": "/js/about.c4e4d8bf.js"
+  },
+  {
+    "revision": "f465dcb24bf57f70020a",
+    "url": "/js/app.0ff15b24.js"
+  },
+  {
+    "revision": "59920d172e1aa44d53bb",
+    "url": "/js/chunk-vendors.65395e02.js"
+  },
+  {
+    "revision": "458db4a1610336368c40e1639ce88594",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
