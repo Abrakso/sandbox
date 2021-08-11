c6f84404d15d2dfed4ababffb8ea583de77f489b
Anatoly Bolgov <bolgov.anatoli@gmail.com>
2021-08-11 09:36:19 +0700

    init

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'My Sandbox',
+    short_name: 'Sandbox',
+    themeColor: '#254699',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
+};
